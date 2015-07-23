0802963ba9c69c22b7b351ff27d8d5761c0df8f7
Chris Trevino <chris.trevino@atsid.com>
2015-07-22 20:12:57 -0700

    Bumping timeout threshold

--- test/test-app.js
+++ test/test-app.js
@@ -5,17 +5,18 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('generator-ats-node-webapp:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({ skipInstall: true })
-      .on('end', done);
-  });
+    before(function (done) {
+        this.timeout(30000);
+        helpers.run(path.join(__dirname, '../generators/app'))
+            .withOptions({skipInstall: true})
+            .on('end', done);
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'package.json',
-      '.editorconfig',
-      '.eslintrc'
-    ]);
-  });
+    it('creates files', function () {
+        assert.file([
+            'package.json',
+            '.editorconfig',
+            '.eslintrc'
+        ]);
+    });
 });
