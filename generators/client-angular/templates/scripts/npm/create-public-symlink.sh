c3e87e2c3d2edbbc2b30f18d257fb6f8625f6160
Chris Trevino <chris.trevino@atsid.com>
2015-07-24 12:21:33 -0700

    #33 - correcting an issue where the public symlink wasn't being correctly created

--- generators/client-angular/templates/scripts/npm/create-public-symlink.sh
+++ generators/client-angular/templates/scripts/npm/create-public-symlink.sh
@@ -4,6 +4,6 @@ if [ ! -e node_modules ]; then
     mkdir -p node_modules
 fi
 
-if [ ! -L node_modules/app ]; then
+if [ ! -L node_modules/public ]; then
     ln -s ../public node_modules/public
 fi
