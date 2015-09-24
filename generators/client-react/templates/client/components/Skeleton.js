d31299d95a71bde7e246436bc1a5e03704b48f17
Chris Trevino <chris.trevino@atsid.com>
2015-09-24 10:28:43 -0700

    Rename step 1

---
+++ generators/client-react/templates/client/components/Skeleton.js
@@ -0,0 +1,83 @@
+const debug = require('debug')('app:components:application');
+const React = require('react/addons');
+
+// Components
+const mui = require('material-ui');
+const AppBar = mui.AppBar;
+const LeftNav = mui.LeftNav;
+const FlatButton = mui.FlatButton;
+const Router = require('react-router');
+
+const Skeleton = React.createClass({
+  propTypes: {
+    children: React.PropTypes.node,
+  },
+
+  contextTypes: {
+    history: Router.PropTypes.history.isRequired,
+    stores: React.PropTypes.object.isRequired,
+  },
+
+  getInitialState() {
+    return { loading: true, user: null };
+  },
+
+  componentDidMount() {
+    this.getStateFromStore();
+  },
+
+  onLeftNavToggle() {
+    return this.refs.leftNav.toggle();
+  },
+
+  onLeftNavChange(e, key, payload) {
+    // Do DOM Diff refresh
+    this.context.history.pushState(null, payload.route);
+  },
+
+  getStateFromStore() {
+    this.state = {projects: [], loading: true};
+    return this.context.stores.users.getCurrentUser()
+      .then((user) => this.setState({user, loading: false}))
+      .catch((err) => {
+        debug('error loading store data', err);
+        this.setState({loading: false});
+      });
+  },
+
+  render() {
+    const menuItems = [
+      {route: '/', text: 'Home'},
+    ];
+
+    if (this.state.user) {
+      menuItems.push({route: '/logout', text: 'Logout'});
+    } else {
+      menuItems.push({route: '/login', text: 'Login'});
+    }
+
+    const appBarRightLabel = this.state.user ? this.state.user.name : 'Login';
+    return (
+      <div>
+        <LeftNav
+          ref="leftNav"
+          menuItems={menuItems}
+          docked={false}
+          onChange={this.onLeftNavChange} />
+
+        <header>
+          <AppBar
+            title="Badger"
+            onLeftIconButtonTouchTap={this.onLeftNavToggle}
+            iconElementRight={<FlatButton label={appBarRightLabel} />} />
+        </header>
+
+        <section className="content">
+          {this.props.children}
+        </section>
+      </div>
+    );
+  },
+});
+
+module.exports = Skeleton;
