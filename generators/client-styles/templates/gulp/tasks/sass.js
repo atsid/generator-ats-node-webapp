aead285d401f201f42b94226bbbcdbc83d646659
Chris Trevino <chris.trevino@atsid.com>
2015-10-02 10:47:12 -0700

    #91 - Update the Sass Task to Use the Glob in the Gulp Config Updating the Gulp Config so that it checks for Sass in the entire client dir. Updating the Gulp Config so that it looks for .sass and .scss files

--- generators/client-styles/templates/gulp/tasks/sass.js
+++ generators/client-styles/templates/gulp/tasks/sass.js
@@ -3,10 +3,8 @@ const sass = require('gulp-sass');
 const lload = require('gulp-livereload');
 const config = require('../config');
 
-const STYLES_GLOB = 'client/styles/**/*.scss';
-
 gulp.task('sass', () => {
-  gulp.src(STYLES_GLOB)
+  gulp.src(config.client.styles)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(config.client.dist.path))
     .pipe(lload());
