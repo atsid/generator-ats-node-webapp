289a824a5a6a9d462029132f959bcbd87a536e19
Chris Trevino <chris.trevino@atsid.com>
2015-10-07 13:53:38 -0700

    #52 - Add CSS Minification, Sourcemaps

--- generators/client-styles/templates/gulp/tasks/sass.js
+++ generators/client-styles/templates/gulp/tasks/sass.js
@@ -1,13 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const lload = require('gulp-livereload');
-const config = require('../config');
 const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const cssmin = require('gulp-cssmin');
+const config = require('../config');
 
 gulp.task('sass', () => {
   gulp.src(config.client.styles)
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(cssmin())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.client.dist.path))
     .pipe(lload());
 });
