9b6b1edacdf284d0db3a8de2ba40196efa8393a8
Chris Trevino <chris.trevino@atsid.com>
2015-10-08 12:02:19 -0700

    #90 - Add 'styles' Task to the 'prepare-assets' Task

--- generators/gulp/templates/gulp/tasks/default.js
+++ generators/gulp/templates/gulp/tasks/default.js
@@ -4,7 +4,6 @@ const runSequence = require('run-sequence');
 gulp.task('default', (cb) => {
   return runSequence(
     'lint',
-    'styles',
     'prepare-assets',
     'bundle-client',
     'test',
