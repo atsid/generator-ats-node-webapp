af2f0f28273fe4a37f9caa360f7505ee66b0a20f
Chris Trevino <chris.trevino@atsid.com>
2015-10-30 13:27:37 -0700

    #42 - Use Nock to Eliminate an Annoying Test Log Message

--- generators/client-react/templates/client/components/Application.spec.js
+++ generators/client-react/templates/client/components/Application.spec.js
@@ -3,9 +3,11 @@ const { expect } = require('chai');
 const React = require('react/addons');
 const ReactTestUtils = React.addons.TestUtils;
 const Application = require('./Application');
+const nock = require('nock');
 
 describe('Application Component', () => {
   it('should load', () => {
+    nock('http://localhost').get('/api/auth/current').reply(200, { 'name': 'Bob Tester'});
     const renderedComponent = ReactTestUtils.renderIntoDocument(
       <Application />
     );
