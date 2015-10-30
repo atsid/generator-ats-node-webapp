981a58ae7ad3ee6933266c4f8426896e738c15e5
Chris Trevino <chris.trevino@atsid.com>
2015-10-30 13:10:44 -0700

    #42 - Add Deleted Test Fixture

--- generators/app-symlink/templates/scripts/npm/create-app-symlink.sh
+++ generators/app-symlink/templates/scripts/npm/create-app-symlink.sh
@@ -4,5 +4,8 @@ if [ ! -e node_modules ]; then
     mkdir -p node_modules
 fi
 
-rm node_modules/app
+if [ -e node_modules/app ]; then
+  rm node_modules/app
+fi
+
 ln -sf `pwd`/server node_modules/app
--- generators/skeleton/templates/scripts/npm/create-public-symlink.sh
+++ generators/skeleton/templates/scripts/npm/create-public-symlink.sh
@@ -4,5 +4,8 @@ if [ ! -e node_modules ]; then
     mkdir -p node_modules
 fi
 
-rm node_modules/public
+if [ -e node_modules/public ]; then
+  rm node_modules/public
+fi
+
 ln -sf `pwd`/public node_modules
